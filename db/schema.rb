e0ee01dd25807a642a559dc152442bf8f89f430a
Olly Legg <olly@new-bamboo.co.uk>
2012-05-17 17:00:31 +0100

    Add Loan model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120517155611) do
+
+  create_table "loans", :force => true do |t|
+    t.boolean  "viable_proposition",                :null => false
+    t.boolean  "would_you_lend",                    :null => false
+    t.boolean  "collateral_exhausted",              :null => false
+    t.integer  "amount",                            :null => false
+    t.integer  "lender_cap",                        :null => false
+    t.integer  "repayment_duration",                :null => false
+    t.integer  "turnover",                          :null => false
+    t.date     "trading_date",                      :null => false
+    t.string   "sic_code",                          :null => false
+    t.integer  "loan_category",                     :null => false
+    t.integer  "reason",                            :null => false
+    t.boolean  "previous_borrowing",                :null => false
+    t.boolean  "private_residence_charge_required", :null => false
+    t.boolean  "personal_guarantee_required",       :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
+  end
 
 end
