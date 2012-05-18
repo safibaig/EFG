652965ab4294a18cf741751652de03f94c78e5f3
Ben Pickles <spideryoung@gmail.com>
2012-05-18 12:59:10 +0100

    Boolean values are valid with true or false.

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -9,9 +9,6 @@ describe Loan do
     end
 
     %w(
-      viable_proposition
-      would_you_lend
-      collateral_exhausted
       amount
       lender_cap
       repayment_duration
@@ -20,13 +17,36 @@ describe Loan do
       sic_code
       loan_category
       reason
-      previous_borrowing
-      private_residence_charge_required
-      personal_guarantee_required
     ).each do |attr|
       it "is invalid without #{attr}" do
         loan[attr] = ''
         loan.should_not be_valid
+      end
+    end
+
+    %w(
+      viable_proposition
+      would_you_lend
+      collateral_exhausted
+      previous_borrowing
+      private_residence_charge_required
+      personal_guarantee_required
+    ).each do |attr|
+      describe "boolean value: #{attr}" do
+        it "is valid with true" do
+          loan[attr] = true
+          loan.should be_valid
+        end
+
+        it "is valid with false" do
+          loan[attr] = false
+          loan.should be_valid
+        end
+
+        it "is invalid with nil" do
+          loan[attr] = nil
+          loan.should be_invalid
+        end
       end
     end
   end
