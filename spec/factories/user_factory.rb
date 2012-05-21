5c3f8791f1aa81f5b57cb007a67c03f71919565b
Olly Legg <olly@new-bamboo.co.uk>
2012-05-21 15:20:38 +0100

    Add name field to users.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    name 'Joe Bloggs'
     email 'joe@example.com'
     password 'password'
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 
 describe User do
 
+  describe "validations" do
+    let(:user) { FactoryGirl.build(:user) }
+
+    it "should have a valid factory" do
+      user.should be_valid
+    end
+
+    it "should require a name" do
+      user.name = ''
+      user.should_not be_valid
+    end
+  end
+
 end
