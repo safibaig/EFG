b4732296b4bce493cb5054f913bdefb02202acd7
Ben Pickles <spideryoung@gmail.com>
2012-05-21 12:02:14 +0100

    Test that a valid loan is actually being created.

--- spec/requests/loans/eligibility_checks_spec.rb
+++ spec/requests/loans/eligibility_checks_spec.rb
@@ -12,7 +12,7 @@ describe 'eligibility checks' do
     choose_radio_button 'lender_cap', 1
     fill_in_duration_input 'repayment_duration', 2, 6
     fill_in 'turnover', 1234567.89
-    fill_in 'trading_date', 2.years.ago.strftime('%d/%m/%Y')
+    fill_in 'trading_date', '31/1/2012'
     fill_in 'sic_code', 'DA15.61/1'
     choose_radio_button 'loan_category', 2
     choose_radio_button 'reason', 3
@@ -20,9 +20,39 @@ describe 'eligibility checks' do
     choose_radio_button 'private_residence_charge_required', false
     choose_radio_button 'personal_guarantee_required', true
 
-    click_button 'Check'
+    expect {
+      click_button 'Check'
+    }.to change(Loan, :count).by(1)
 
-    current_path.should == loan_path(Loan.last)
+    loan = Loan.last
+
+    current_path.should == loan_path(loan)
+
+    loan.viable_proposition.should be_true
+    loan.would_you_lend.should be_true
+    loan.collateral_exhausted.should be_true
+    loan.amount.should == 123456789
+    loan.lender_cap.should == 1
+    loan.repayment_duration.should == 30
+    loan.turnover.should == 123456789
+    loan.trading_date.should == Date.new(2012, 1, 31)
+    loan.sic_code.should == 'DA15.61/1'
+    loan.loan_category.should == 2
+    loan.reason.should == 3
+    loan.previous_borrowing.should be_true
+    loan.private_residence_charge_required.should be_false
+    loan.personal_guarantee_required.should be_true
+  end
+
+  it 'does not create an invalid loan' do
+    visit root_path
+    click_link 'Check Eligibility'
+
+    expect {
+      click_button 'Check'
+    }.not_to change(Loan, :count)
+
+    current_path.should == '/loans/eligibility_check'
   end
 
   private
