967460fd18046cd2bf48ed9caecfeb296ce802d4
Ben Pickles <spideryoung@gmail.com>
2012-05-24 17:10:46 +0100

    Parse facility letter date nicely.

--- app/models/loan_offer.rb
+++ app/models/loan_offer.rb
@@ -3,7 +3,11 @@ require 'active_model/model'
 class LoanOffer
   include ActiveModel::Model
 
-  ATTRIBUTES = [:facility_letter_date, :facility_letter_sent]
+  READ_ONLY_ATTRIBUTES = [:facility_letter_date]
+
+  delegate *READ_ONLY_ATTRIBUTES, to: :loan
+
+  ATTRIBUTES = [:facility_letter_sent]
 
   ATTRIBUTES.each do |attribute|
     delegate attribute, "#{attribute}=", to: :loan
@@ -15,5 +19,13 @@ class LoanOffer
   def initialize(loan, attributes = {})
     @loan = loan
     super(attributes)
+  end
+
+  def facility_letter_date=(value)
+    match = value.match(%r{(\d+)/(\d+)/(\d+)})
+    return unless match
+    day, month, year = match[1..3].map(&:to_i)
+    year += 2000 if year < 2000
+    loan.facility_letter_date = Date.new(year, month, day)
   end
 end
