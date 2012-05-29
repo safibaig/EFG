82b85a5f32ce506976dc43755d75ee930968a0cc
Ben Pickles <spideryoung@gmail.com>
2012-05-29 12:25:27 +0100

    List loan count by state.

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,4 +1,13 @@
 class LoansController < ApplicationController
+  def index
+    @states = Loan::States.map { |state|
+      OpenStruct.new(
+        loan_count: current_lender.loans.where(state: state).count,
+        name: state
+      )
+    }
+  end
+
   def show
     @loan = current_lender.loans.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ EFG::Application.routes.draw do
 
   root to: 'dashboard#show'
 
-  resources :loans, only: :show do
+  resources :loans, only: [:index, :show] do
     collection do
       resource :eligibility_check, only: [:new, :create]
     end
