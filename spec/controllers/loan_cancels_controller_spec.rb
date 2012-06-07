1c833aef68cb46fbe929af41ea5ae2b2ed102a27
Ben Pickles <spideryoung@gmail.com>
2012-06-07 17:37:05 +0100

    Loan cancellation form.

---
+++ spec/controllers/loan_cancels_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe LoanCancelsController do
+  describe '#new' do
+    let(:current_lender) { FactoryGirl.create(:lender) }
+    let(:current_user) { FactoryGirl.create(:user, lender: current_lender) }
+    before { sign_in(current_user) }
+
+    def dispatch(params)
+      get :new, params
+    end
+
+    it 'works with a loan from the same lender' do
+      loan = FactoryGirl.create(:loan, :eligible, lender: current_lender)
+
+      dispatch loan_id: loan.id
+
+      response.should be_success
+    end
+
+    it 'raises RecordNotFound for a loan from another lender' do
+      other_lender = FactoryGirl.create(:lender)
+      loan = FactoryGirl.create(:loan, :eligible, lender: other_lender)
+
+      expect {
+        dispatch loan_id: loan.id
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe '#create' do
+    let(:current_lender) { FactoryGirl.create(:lender) }
+    let(:current_user) { FactoryGirl.create(:user, lender: current_lender) }
+    before { sign_in(current_user) }
+
+    def dispatch(params = {})
+      default_params = { loan_id: loan.id, loan_cancel: {} }
+      post :create, default_params.merge(params)
+    end
+
+    context "with another lender's loan" do
+      let(:other_lender) { FactoryGirl.create(:lender) }
+      let(:loan) { FactoryGirl.create(:loan, :eligible, lender: other_lender) }
+
+      it 'raises RecordNotFound for a loan from another lender' do
+        expect {
+          dispatch
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+end
---
+++ spec/requests/loans/loan_cancel_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'loan cancel' do
+  let(:current_lender) { FactoryGirl.create(:lender) }
+  let(:current_user) { FactoryGirl.create(:user, lender: current_lender) }
+  let(:loan) { FactoryGirl.create(:loan, :eligible, lender: current_lender) }
+  before { login_as(current_user, scope: :user) }
+
+  it 'entering further loan information' do
+    visit loan_path(loan)
+    click_link 'Cancel Loan'
+
+    fill_in 'cancelled_on', '1/6/2012'
+    choose_radio_button 'cancelled_reason', 4
+    fill_in 'cancelled_comment', 'No comment'
+
+    click_button 'Submit'
+
+    loan = Loan.last
+
+    current_path.should == loan_path(loan)
+
+    loan.state.should == Loan::Cancelled
+    loan.cancelled_on.should == Date.new(2012, 6, 1)
+    loan.cancelled_reason.should == 4
+    loan.cancelled_comment.should == 'No comment'
+  end
+
+  it 'does not continue with invalid values' do
+    visit loan_path(loan)
+    click_link 'Cancel Loan'
+
+    loan.state.should == Loan::Eligible
+    expect {
+      click_button 'Submit'
+      loan.reload
+    }.to_not change(loan, :state)
+
+    current_path.should == loan_cancel_path(loan)
+  end
+
+  private
+  def choose_radio_button(attribute, value)
+    choose "loan_cancel_#{attribute}_#{value}"
+  end
+
+  def fill_in(attribute, value)
+    page.fill_in "loan_cancel_#{attribute}", with: value
+  end
+end
