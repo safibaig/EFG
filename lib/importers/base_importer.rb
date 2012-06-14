87a2e10e6d7f7a2007e820ff7afe4e8e286f961f
Ben Pickles <spideryoung@gmail.com>
2012-06-14 16:19:26 +0100

    Use Sequel for importing CSVs. Break some tests.

--- lib/importers/base_importer.rb
+++ lib/importers/base_importer.rb
@@ -1,34 +1,51 @@
 require 'csv'
+require 'sequel'
 
 class BaseImporter
   class << self
     attr_accessor :csv_path
-    attr_accessor :klass
+    attr_accessor :table_name
   end
 
-  def initialize(row, row_number)
+  DB = Sequel.connect(YAML.load_file(Rails.root.join('config/database.yml'))[Rails.env])
+
+  attr_accessor :attributes, :row
+
+  def initialize(row)
     @row = row
-    @row_number = row_number
+    parse_row
+  end
+
+  def self.bulk_import(values)
+    DB[table_name].import(columns, values)
+    values.clear
+  end
+
+  def self.columns
+    field_mapping.values
+  end
+
+  def self.field_mapping
+    {}
   end
 
   def self.import
-    index = 0
     CSV.foreach(csv_path, headers: true) do |row|
-      attributes = new(row, index).attributes
-      model = klass.new
-
-      attributes.each do |key, value|
-        setter_method = "#{key}="
-        model.respond_to?(setter_method) ? model.send(setter_method, value) : model[key] = value
-      end
-
-      model.save!
-      index += 1
+      values << new(row).values
+      bulk_import(values) if values.length % 1000 == 0
     end
-    after_import
+
+    bulk_import(values)
   end
 
-  def self.after_import
+  def parse_row
+    self.attributes = row.inject({}) { |memo, (name, value)|
+      memo[self.class.field_mapping[name]] = value
+      memo
+    }
   end
 
+  def values
+    attributes.values
+  end
 end
--- lib/importers/lender_importer.rb
+++ lib/importers/lender_importer.rb
@@ -1,41 +1,30 @@
 class LenderImporter < BaseImporter
   self.csv_path = Rails.root.join('import_data/lenders.csv')
-  self.klass = Lender
+  self.table_name = :lenders
 
-  FIELD_MAPPING = {
-    "OID"                         => :legacy_id,
-    "NAME"                        => :name,
-    "CREATION_TIME"               => :created_at,
-    "LAST_MODIFIED"               => :updated_at,
-    "VERSION"                     => :version,
-    "HIGH_VOLUME"                 => :high_volume,
-    "CAN_USE_ADD_CAP"             => :can_use_add_cap,
-    "ORGANISATION_REFERENCE_CODE" => :organisation_reference_code,
-    "PRIMARY_CONTACT_NAME"        => :primary_contact_name,
-    "PRIMARY_CONTACT_PHONE"       => :primary_contact_phone,
-    "PRIMARY_CONTACT_EMAIL"       => :primary_contact_email,
-    "STD_CAP_LENDING_ALLOCATION"  => :std_cap_lending_allocation,
-    "ADD_CAP_LENDING_ALLOCATION"  => :add_cap_lending_allocation,
-    "DISABLED"                    => :disabled,
-    "AR_TIMESTAMP"                => :ar_timestamp,
-    "AR_INSERT_TIMESTAMP"         => :ar_insert_timestamp,
-    "CREATED_BY"                  => :created_by,
-    "MODIFIED_BY"                 => :modified_by,
-    "ALLOW_ALERT_PROCESS"         => :allow_alert_process,
-    "MAIN_POINT_OF_CONTACT_USER"  => :main_point_of_contact_user,
-    "LOAN_SCHEME"                 => :loan_scheme
-  }
-
-  def attributes
-    attrs = {}
-    @row.each do |field_name, value|
-      attrs[FIELD_MAPPING[field_name]] = value
-    end
-    attrs
-  end
-  
-  private
-  
-  def self.after_import
+  def self.field_mapping
+    {
+      "OID"                         => :legacy_id,
+      "NAME"                        => :name,
+      "CREATION_TIME"               => :created_at,
+      "LAST_MODIFIED"               => :updated_at,
+      "VERSION"                     => :version,
+      "HIGH_VOLUME"                 => :high_volume,
+      "CAN_USE_ADD_CAP"             => :can_use_add_cap,
+      "ORGANISATION_REFERENCE_CODE" => :organisation_reference_code,
+      "PRIMARY_CONTACT_NAME"        => :primary_contact_name,
+      "PRIMARY_CONTACT_PHONE"       => :primary_contact_phone,
+      "PRIMARY_CONTACT_EMAIL"       => :primary_contact_email,
+      "STD_CAP_LENDING_ALLOCATION"  => :std_cap_lending_allocation,
+      "ADD_CAP_LENDING_ALLOCATION"  => :add_cap_lending_allocation,
+      "DISABLED"                    => :disabled,
+      "AR_TIMESTAMP"                => :ar_timestamp,
+      "AR_INSERT_TIMESTAMP"         => :ar_insert_timestamp,
+      "CREATED_BY"                  => :created_by,
+      "MODIFIED_BY"                 => :modified_by,
+      "ALLOW_ALERT_PROCESS"         => :allow_alert_process,
+      "MAIN_POINT_OF_CONTACT_USER"  => :main_point_of_contact_user,
+      "LOAN_SCHEME"                 => :loan_scheme
+    }
   end
 end
