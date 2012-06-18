8bf0b194c94dbd5ceec3d5b042e613aa4f9b5668
James Abley <james.abley@gmail.com>
2012-06-18 14:22:53 +0100

    More idiomatic ruby
    Learning as I go!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,12 @@
-lender = Lender.find_or_create_by_name('Banksy')
+lender = Lender.find_or_create_by_name("Banksy")
 
-user = User.find_by_email('bob@example.com')
-
-if !user
-  user = User.new
-  user.lender = lender
-  user.first_name = 'Bob'
-  user.last_name = 'Flemming'
-  user.email = 'bob@example.com'
-  user.password = user.password_confirmation = 'password'
-  user.save!
-end
+user = User.find_or_create_by_email(:email => "bob@example.com",
+  :lender => lender,
+  :first_name => "Bob",
+  :last_name => "Flemming",
+  :password => "password",
+  :password_confirmation => "password"
+  )
 
 puts 'Sign-in as:'
 puts "  email: #{user.email}"
