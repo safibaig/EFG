92ae7509197864b21e9a8a82c7795c3fddb36145
James Abley <james.abley@gmail.com>
2012-06-18 14:46:33 +0100

    Prefer Ruby 1.9 hash syntax
    As per our coding standards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 lender = Lender.find_or_create_by_name("Banksy")
 
-user = User.find_or_create_by_email(:email => "bob@example.com",
-  :lender => lender,
-  :first_name => "Bob",
-  :last_name => "Flemming",
-  :password => "password",
-  :password_confirmation => "password"
+user = User.find_or_create_by_email(email: "bob@example.com",
+  lender: lender,
+  first_name: "Bob",
+  last_name: "Flemming",
+  password: "password",
+  password_confirmation: "password"
   )
 
 puts 'Sign-in as:'
