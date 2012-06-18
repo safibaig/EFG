8dc38f7bdbc7fc3ddfcdfeea35f20894cbab7d8f
James Abley <james.abley@gmail.com>
2012-06-18 14:14:22 +0100

    Make db seed idempotent
    Just helps for when I've forgotten what's been run and what hasn't.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
-lender = Lender.new
-lender.name = 'Banksy'
-lender.save!
+lender = Lender.find_or_create_by_name('Banksy')
 
-user = User.new
-user.lender = lender
-user.first_name = 'Bob'
-user.last_name = 'Flemming'
-user.email = 'bob@example.com'
-user.password = user.password_confirmation = 'password'
-user.save!
+user = User.find_by_email('bob@example.com')
+
+if !user
+  user = User.new
+  user.lender = lender
+  user.first_name = 'Bob'
+  user.last_name = 'Flemming'
+  user.email = 'bob@example.com'
+  user.password = user.password_confirmation = 'password'
+  user.save!
+end
 
 puts 'Sign-in as:'
 puts "  email: #{user.email}"
