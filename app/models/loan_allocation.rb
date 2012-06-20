6be80dab969c476d9384153b2d3999540ef6ee68
Niall Mullally <niall.mullally@gmail.com>
2012-06-20 15:09:20 +0100

    LoanAllocation#completed_loans now returns only guaranteed loans

--- app/models/loan_allocation.rb
+++ app/models/loan_allocation.rb
@@ -8,7 +8,7 @@ class LoanAllocation < ActiveRecord::Base
   # FIXME: update to use the correct loan states
   has_many :completed_loans,
            :class_name => "Loan",
-           :conditions => ["loans.state IN (?)", [Loan::Offered, Loan::Repaid, Loan::NotDemanded, Loan::Demanded, Loan::Completed, Loan::Offered, Loan::Eligible, Loan::AutoCancelled]]
+           :conditions => ["loans.state IN (?)", [Loan::Guaranteed]]
 
   validates_presence_of :lender_id, strict: true
   validates_presence_of :allocation, :starts_on, :ends_on
