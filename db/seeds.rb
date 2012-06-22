5707f9dc0499caa0d2a764f60755eafcc0930c2f
James Abley <james.abley@gmail.com>
2012-06-22 14:13:03 +0100

    Fixed my breakage with database seeding
    lender association wasn't being populated and the app was erroring
    after sign-in.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 lender = Lender.find_or_create_by_name("Banksy")
+lender.save!
 
 user = User.find_or_create_by_email(email: "bob@example.com",
-  lender: lender,
   first_name: "Bob",
   last_name: "Flemming",
   password: "password",
   password_confirmation: "password"
   )
+
+user.lender = lender
+user.save!
 
 puts 'Sign-in as:'
 puts "  email: #{user.email}"
