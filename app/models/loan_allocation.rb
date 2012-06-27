68cc0590da3c72a97f9f034674e0759bfdf98bcc
Olly Legg <olly@new-bamboo.co.uk>
2012-06-27 17:49:30 +0100

    Move Loan Allocation title to the model.

--- app/models/loan_allocation.rb
+++ app/models/loan_allocation.rb
@@ -15,4 +15,9 @@ class LoanAllocation < ActiveRecord::Base
 
   format :allocation, with: MoneyFormatter
 
+  def title
+    start_date = starts_on.strftime('%B %Y')
+    end_date = ends_on.strftime('%B %Y')
+    [start_date, end_date].join(' - ')
+  end
 end
