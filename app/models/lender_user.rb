5a2888a4fd8bac28e1e9e740351cb21bee569c50
Ben Pickles <spideryoung@gmail.com>
2012-07-03 16:17:48 +0100

    LenderUser requires a Lender.

--- app/models/lender_user.rb
+++ app/models/lender_user.rb
@@ -1,6 +1,5 @@
 class LenderUser < User
   belongs_to :lender
 
-  # FIXME: lender users should require a lender so require lender_id when we
-  # come to implementing roles
+  validates_presence_of :lender
 end
--- spec/models/lender_user_spec.rb
+++ spec/models/lender_user_spec.rb
@@ -4,4 +4,11 @@ describe LenderUser do
   let(:user) { FactoryGirl.build(:lender_user) }
 
   it_behaves_like 'User'
+
+  describe 'validations' do
+    it 'requires a lender' do
+      user.lender = nil
+      user.should_not be_valid
+    end
+  end
 end
