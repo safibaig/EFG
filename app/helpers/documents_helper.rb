f5b4b3b6f0b555d0a951c7dde9c92099bde9b29b
Ben Pickles <spideryoung@gmail.com>
2012-07-05 12:48:44 +0100

    More natural Rails name.

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,7 +1,5 @@
 module DocumentsHelper
-
-  def state_aid_letter_link(loan)
+  def link_to_state_aid_letter(loan)
     link_to 'Generate State Aid Letter', state_aid_letter_document_path(@loan, format: :pdf), class: 'btn btn-info'
   end
-
 end
