81a69cc6856a9c9ed8af8741a00e69eb7400a40d
Ben Pickles <spideryoung@gmail.com>
2012-07-05 14:09:50 +0100

    Protect documents controller.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,6 +3,7 @@ class DocumentsController < ApplicationController
   respond_to :pdf
 
   def state_aid_letter
+    enforce_view_permission(StateAidLetter)
     loan = current_lender.loans.find(params[:id])
     pdf = StateAidLetter.new(loan)
 
@@ -10,6 +11,7 @@ class DocumentsController < ApplicationController
   end
 
   def information_declaration
+    enforce_view_permission(InformationDeclaration)
     loan = current_lender.loans.find(params[:id])
     pdf = InformationDeclaration.new(loan)
 
@@ -17,6 +19,7 @@ class DocumentsController < ApplicationController
   end
 
   def data_protection_declaration
+    enforce_view_permission(DataProtectionDeclaration)
     pdf = DataProtectionDeclaration.new
 
     send_data(pdf.render, filename: pdf.filename, type: "application/pdf", disposition: "inline")
--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -1,36 +1,32 @@
 require 'spec_helper'
 
 describe DocumentsController do
-
-  let(:current_lender) { FactoryGirl.create(:lender) }
-
-  let(:current_user) { FactoryGirl.create(:lender_user, lender: current_lender) }
-
+  let(:loan) { FactoryGirl.create(:loan, :completed) }
+  let(:current_user) { FactoryGirl.create(:lender_user, lender: loan.lender) }
   before { sign_in(current_user) }
 
   describe '#state_aid_letter' do
-
-    def dispatch(params)
-      get :state_aid_letter, params
+    def dispatch
+      get :state_aid_letter, id: loan.id
     end
 
     it_behaves_like 'documents controller action'
   end
 
   describe '#information_declaration' do
-
-    def dispatch(params)
-      get :information_declaration, params
+    def dispatch
+      get :information_declaration, id: loan.id
     end
 
     it_behaves_like 'documents controller action'
   end
 
   describe '#data_protection_declaration' do
-
     def dispatch
-      get :data_protection_declaration
+      get :data_protection_declaration, id: loan.id
     end
+
+    it_behaves_like 'CfeUser-restricted LoanPresenter controller'
 
     it 'renders PDF document' do
       dispatch
@@ -38,5 +34,4 @@ describe DocumentsController do
       response.content_type.should == 'application/pdf'
     end
   end
-
 end
