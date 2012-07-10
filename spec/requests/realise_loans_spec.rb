c97b860b674ee9a207127cad09e42284891c012f
Niall Mullally <niall.mullally@gmail.com>
2012-07-10 17:37:48 +0100

    Request spec for realising loans

---
+++ spec/requests/realise_loans_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Realise loans' do
+
+  let(:current_user) { FactoryGirl.create(:cfe_user) }
+
+  let!(:loan) { FactoryGirl.create(:loan, :recovered) }
+
+  before do
+    login_as(current_user, scope: :user)
+  end
+
+  it 'should realise recovered loans' do
+    visit root_path
+    click_link 'Recoveries statement received'
+
+    select loan.lender.name, from: 'realisation_statement_lender_id'
+    fill_in 'realisation_statement_reference', with: "ABC123"
+    select 'March', from: 'realisation_statement_period_covered_quarter'
+    fill_in 'realisation_statement_period_covered_year', with: '2011'
+    fill_in 'realisation_statement_received_on', with: '20/05/2011'
+    click_button 'Select Loans'
+
+    check 'realisation_statement[loans_to_be_realised_ids][]'
+    click_button 'Realise Loans'
+
+    page.should have_content('The following loans have been realised')
+    page.should have_content(loan.reference)
+
+    loan.reload.state.should == Loan::Realised
+  end
+
+end
