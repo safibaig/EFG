be35beb2839610be937dead78ad0517263f097af
Niall Mullally <niall.mullally@gmail.com>
2012-07-11 12:17:28 +0100

    Request spec for removeing guarantee from loan

---
+++ spec/requests/remove_guarantee_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'Remove guarantee' do
+
+  let(:current_user) { FactoryGirl.create(:cfe_user) }
+
+  let!(:loan) { FactoryGirl.create(:loan, :guaranteed) }
+
+  before do
+    login_as(current_user, scope: :user)
+  end
+
+  it 'should remove guarantee from loan' do
+    visit loan_path(loan)
+    click_link 'Remove guarantee'
+
+    fill_in 'remove_guarantee_remove_guarantee_on', with: '20/05/2012'
+    fill_in 'remove_guarantee_remove_guarantee_outstanding_amount', with: '10000'
+    fill_in 'remove_guarantee_remove_guarantee_reason', with: 'n/a/'
+    click_button 'Remove Guarantee'
+
+    page.should have_content('The Guarantee has been removed in respect of this facility.')
+    loan.reload.state.should == 'removed'
+  end
+
+end
