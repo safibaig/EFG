f2a8b67a9d1160d4eab150e5981f3b72e8e53f75
Niall Mullally <niall.mullally@gmail.com>
2012-07-11 14:59:24 +0100

    Fix remove guarantee request spec

--- spec/requests/remove_guarantee_spec.rb
+++ spec/requests/remove_guarantee_spec.rb
@@ -14,9 +14,9 @@ describe 'Remove guarantee' do
     visit loan_path(loan)
     click_link 'Remove guarantee'
 
-    fill_in 'remove_guarantee_remove_guarantee_on', with: '20/05/2012'
-    fill_in 'remove_guarantee_remove_guarantee_outstanding_amount', with: '10000'
-    fill_in 'remove_guarantee_remove_guarantee_reason', with: 'n/a/'
+    fill_in 'loan_remove_guarantee_remove_guarantee_on', with: '20/05/2012'
+    fill_in 'loan_remove_guarantee_remove_guarantee_outstanding_amount', with: '10000'
+    fill_in 'loan_remove_guarantee_remove_guarantee_reason', with: 'n/a/'
     click_button 'Remove Guarantee'
 
     page.should have_content('The Guarantee has been removed in respect of this facility.')
