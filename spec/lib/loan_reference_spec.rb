a78b814385674070c39333c11d639aef61d5d877
Niall Mullally <niall.mullally@gmail.com>
2012-07-12 12:02:49 +0100

    Tweak loan reference specs

--- spec/lib/loan_reference_spec.rb
+++ spec/lib/loan_reference_spec.rb
@@ -5,16 +5,16 @@ describe LoanReference do
   describe '#generate' do
     let(:loan) { FactoryGirl.build(:loan) }
 
-    it "should generate reference in format {letters/numbers}{separator}{version number}" do
+    it "should return reference in format {letters/numbers}{separator}{version number}" do
       reference = LoanReference.generate
 
       reference[0,6].should match(/\d*[A-Z]*/)
       reference[-3,3].should match(/\+\d{2}/)
     end
 
-    it "should be unique" do
+    it "should return unique reference" do
       LoanReference.stub(:create_reference_string).and_return('ABC123', 'DEF123')
-      another_loan = FactoryGirl.create(:loan, reference: 'ABC123')
+      existing_loan = FactoryGirl.create(:loan, reference: 'ABC123')
 
       loan.save!
 
