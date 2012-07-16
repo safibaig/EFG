f61c4da4c63d2df78963edf2bf71e7e681f64732
Niall Mullally <niall.mullally@gmail.com>
2012-07-16 12:30:04 +0100

    Add TransferredLoanEntry presenter

---
+++ spec/factories/transferred_loan_entry_factory.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :transferred_loan_entry do
+    declaration_signed true
+    amount 50000
+    repayment_duration({ months: 24 })
+    repayment_frequency_id 1
+    maturity_date '01/01/2012'
+
+    initialize_with {
+      state_aid_calculation = FactoryGirl.build(:state_aid_calculation)
+      loan = FactoryGirl.build(:loan, :incomplete, state_aid_calculation: state_aid_calculation)
+      new(loan)
+    }
+  end
+end
