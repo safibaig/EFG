4dd5375cf6530e77f73fef692c1607de509d5bde
James Abley <james.abley@digital.cabinet-office.gov.uk>
2012-07-17 14:38:47 +0100

    Fix [#32284471] alert dates should not be constants

--- app/concerns/loan_alerts.rb
+++ app/concerns/loan_alerts.rb
@@ -1,26 +1,54 @@
 module LoanAlerts
   extend ActiveSupport::Concern
 
-  NotProgressedStartDate = 183.days.ago.to_date
-  NotProgressedEndDate = 124.days.ago.to_date
+  def not_progressed_start_date
+    183.days.ago.to_date
+  end
 
-  NotDrawnStartDate = 183.days.ago.to_date
-  NotDrawnEndDate = 124.days.ago.to_date
+  def not_progressed_end_date
+    124.days.ago.to_date
+  end
 
-  DemandedStartDate = 365.days.ago.to_date
-  DemandedEndDate = 306.days.ago.to_date
+  def not_drawn_start_date
+    183.days.ago.to_date
+  end
 
-  AssumedRepaidOfferedStartDate = 183.days.ago.to_date
-  AssumedRepaidOfferedEndDate = 124.days.ago.to_date
+  def not_drawn_end_date
+    124.days.ago.to_date
+  end
 
-  AssumedRepaidGuaranteedStartDate = 92.days.ago.to_date
-  AssumedRepaidGuaranteedEndDate = 33.days.ago.to_date
+  def demanded_start_date
+    365.days.ago.to_date
+  end
+
+  def demanded_end_date
+    306.days.ago.to_date
+  end
+
+  def assumed_repaid_offered_start_date
+    183.days.ago.to_date
+  end
+
+  def assumed_repaid_offered_end_date
+    124.days.ago.to_date
+  end
+
+  def assumed_repaid_guaranteed_start_date
+    92.days.ago.to_date
+  end
+
+  def assumed_repaid_guaranteed_end_date
+    33.days.ago.to_date
+  end
 
   # From 'CfEL Response to Initial Questions.docx':
   # "All schemes, any loan that has remained at the state of
   # “eligible” / “incomplete” or “complete”
   # – for a period of 183 days from entering those states – should be ‘auto cancelled’"
-  def not_progressed_loans(start_date = NotProgressedStartDate, end_date = NotProgressedEndDate)
+  def not_progressed_loans(start_date = nil, end_date = nil)
+    start_date ||= not_progressed_start_date
+    end_date ||= not_progressed_end_date
+
     current_lender.loans.
       not_progressed.
       last_updated_between(start_date, end_date).
@@ -30,7 +58,10 @@ module LoanAlerts
   # From 'CfEL Response to Initial Questions.docx':
   # "Offered loans have 183 days to progress from offered to guaranteed state
   # – if not they progress to auto cancelled""
-  def not_drawn_loans(start_date = NotDrawnStartDate, end_date = NotDrawnEndDate)
+  def not_drawn_loans(start_date = nil, end_date = nil)
+    start_date ||= not_drawn_start_date
+    end_date ||= not_drawn_end_date
+
     current_lender.loans.
       offered.
       last_updated_between(start_date, end_date).
@@ -44,7 +75,10 @@ module LoanAlerts
   #
   # TODO: "EFG loans however, should not be subjected to this alert
   # – they should remain at lender demand indefinitely until such time that the lender themselves progress them"
-  def demanded_loans(start_date = DemandedStartDate, end_date = DemandedEndDate)
+  def demanded_loans(start_date = nil, end_date = nil)
+    start_date ||= demanded_start_date
+    end_date ||= demanded_end_date
+
     current_lender.loans.
       demanded.
       last_updated_between(start_date, end_date).
@@ -63,14 +97,20 @@ module LoanAlerts
   # - state ‘guaranteed’ and maturity date elapsed by 92 days
   #
   # TODO: revisit the criteria for assumed repaid alert group and confirm if it needs further updates
-  def assumed_repaid_offered_loans(start_date = AssumedRepaidOfferedStartDate, end_date = AssumedRepaidOfferedEndDate)
+  def assumed_repaid_offered_loans(start_date = nil, end_date = nil)
+    start_date ||= assumed_repaid_offered_start_date
+    end_date ||= assumed_repaid_offered_end_date
+
     current_lender.loans.
       where(state: [Loan::Incomplete, Loan::Completed, Loan::Offered]).
       maturity_date_between(start_date, end_date).
       order(:updated_at)
   end
 
-  def assumed_repaid_guaranteed_loans(start_date = AssumedRepaidGuaranteedStartDate, end_date = AssumedRepaidGuaranteedEndDate)
+  def assumed_repaid_guaranteed_loans(start_date = nil, end_date = nil)
+    start_date ||= assumed_repaid_guaranteed_start_date
+    end_date ||= assumed_repaid_guaranteed_end_date
+
     current_lender.loans.
       guaranteed.
       maturity_date_between(start_date, end_date).
