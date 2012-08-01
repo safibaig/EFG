3e0a92c5cbc251c9dd42b56ed52c9aaa5e8f71f1
Niall Mullally <niall.mullally@gmail.com>
2012-08-01 17:00:18 +0100

    Update lender_ids form field to be a hidden field with a single lender ID when user has only 1 lender (i.e. lender user) and a multiple select field when user has multiple lenders (i.e. cfe user)

--- app/helpers/loan_reports_helper.rb
+++ app/helpers/loan_reports_helper.rb
@@ -12,6 +12,14 @@ module LoanReportsHelper
     [ ['All'], ['SFLG Only', Loan::SFLG_SCHEME], ['EFG Only', Loan::EFG_SCHEME] ]
   end
 
+  def loan_report_lender_field(form_builder)
+    if current_user.lenders.count == 1
+      hidden_field_tag 'loan_report[lender_ids][]', current_user.lender.id
+    else
+      form_builder.input :lender_ids, as: :select, collection: loan_report_lender_options, input_html: { multiple: true }
+    end
+  end
+
   def loan_report_lender_options
     current_user.lenders.map { |lender| [ lender.name, lender.id ] }
   end
