3dad98c553809370a6bca0f5c35106e12b667c61
Niall Mullally <niall.mullally@gmail.com>
2012-08-03 12:19:43 +0100

    LoanReportController tidying

--- app/controllers/loan_reports_controller.rb
+++ app/controllers/loan_reports_controller.rb
@@ -5,7 +5,7 @@ class LoanReportsController < ApplicationController
   end
 
   def create
-    @loan_report = LoanReport.new(params[:loan_report].merge(allowed_lender_ids: current_user.lenders.collect(&:id)))
+    @loan_report = LoanReport.new(loan_report_params)
     if @loan_report.valid?
       respond_to do |format|
         format.html { render 'summary' }
@@ -18,6 +18,14 @@ class LoanReportsController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  # ensure allowed_lender_ids is included in loan_report params
+  def loan_report_params
+    allowed_lender_ids = current_user.lenders.collect(&:id)
+    params[:loan_report].merge(allowed_lender_ids: allowed_lender_ids)
   end
 
 end
