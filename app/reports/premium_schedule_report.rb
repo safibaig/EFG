1fa4213e34e5bc83a739fa5ffafddc81a50bdf45
Ben Pickles <spideryoung@gmail.com>
2012-08-03 17:44:39 +0100

    Include premiums in CSV.

--- app/reports/premium_schedule_report.rb
+++ app/reports/premium_schedule_report.rb
@@ -33,7 +33,8 @@ class PremiumScheduleReport
         'guaranteed_loan_change.date_of_change AS _guaranteed_date',
         'first_loan_change.date_of_change AS _draw_down_date',
         'loans.*', # TODO: restrict to required columns.
-        'lenders.organisation_reference_code AS _lender_organisation'
+        'lenders.organisation_reference_code AS _lender_organisation',
+        'state_aid_calculations.id AS state_aid_calculation_id'
       ].join(', '))
       .joins(:lender)
       .joins('INNER JOIN loan_changes AS guaranteed_loan_change ON guaranteed_loan_change.loan_id = loans.id')
--- app/reports/premium_schedule_report_row.rb
+++ app/reports/premium_schedule_report_row.rb
@@ -3,11 +3,17 @@ class PremiumScheduleReportRow
     'Schedule Type', 'Initial Premium Cheque', '1st Collection Date',
     'No of Payments'] + (1..40).map { |i| "Premium#{i}" }
 
-  attr_accessor :loan
+  attr_accessor :loan, :state_aid_calculation
 
   def self.from_loans(loans)
+    state_aid_calculation_ids = loans.map(&:state_aid_calculation_id)
+    state_aid_calculations = StateAidCalculation.find(state_aid_calculation_ids)
+    state_aid_calculation_lookup = state_aid_calculations.index_by(&:loan_id)
+
     loans.map do |loan|
-      new(loan)
+      new(loan).tap do |row|
+        row.state_aid_calculation = state_aid_calculation_lookup[loan.id]
+      end
     end
   end
 
@@ -20,10 +26,10 @@ class PremiumScheduleReportRow
       loan._draw_down_date.strftime('%d-%m-%Y'),
       loan._lender_organisation,
       loan.reference,
-      loan.state_aid_calculations.last.calc_type,
+      state_aid_calculation.calc_type,
       nil,  # Initial Premium Cheque
       nil,  # 1st Collection Date
       nil   # No of Payments
-    ] + Array.new(40)
+    ] + state_aid_calculation.premium_schedule.premiums.map(&:to_f)
   end
 end
