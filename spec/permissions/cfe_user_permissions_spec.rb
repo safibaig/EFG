fd37ae58cb397a9bfb7b7e84f185309d7768f7a6
Ben Pickles <spideryoung@gmail.com>
2012-08-07 13:23:59 +0100

    Test the actual user objects, not just that the module logic is correct.

--- spec/permissions/cfe_user_permissions_spec.rb
+++ spec/permissions/cfe_user_permissions_spec.rb
@@ -2,106 +2,107 @@ require 'spec_helper'
 
 describe CfeUserPermissions do
   include RefuteMacro
-  include CfeUserPermissions
+
+  let(:user) { FactoryGirl.build(:cfe_user) }
 
   context 'invoices' do
-    it { assert can_view?(Invoice) }
-    it { assert can_create?(Invoice) }
+    it { assert user.can_view?(Invoice) }
+    it { assert user.can_create?(Invoice) }
   end
 
   context 'recoveries' do
-    it { refute can_create?(Recovery) }
+    it { refute user.can_create?(Recovery) }
   end
 
   context 'realisation statements' do
-    it { assert can_view?(RealisationStatement) }
-    it { assert can_create?(RealisationStatement) }
+    it { assert user.can_view?(RealisationStatement) }
+    it { assert user.can_create?(RealisationStatement) }
   end
 
   context 'remove guarantee' do
-    it { assert can_view?(LoanRemoveGuarantee) }
-    it { assert can_create?(LoanRemoveGuarantee) }
+    it { assert user.can_view?(LoanRemoveGuarantee) }
+    it { assert user.can_create?(LoanRemoveGuarantee) }
   end
 
   context 'loan eligibility checks' do
-    it { refute can_create?(LoanEligibilityCheck) }
+    it { refute user.can_create?(LoanEligibilityCheck) }
   end
 
   context 'state aid calculations' do
-    it { refute can_update?(StateAidCalculation) }
+    it { refute user.can_update?(StateAidCalculation) }
   end
 
   context 'data protection declaration' do
-    it { refute can_view?(DataProtectionDeclaration) }
+    it { refute user.can_view?(DataProtectionDeclaration) }
   end
 
   context 'information declaration' do
-    it { refute can_view?(InformationDeclaration) }
+    it { refute user.can_view?(InformationDeclaration) }
   end
 
   context 'state aid letters' do
-    it { refute can_view?(StateAidLetter) }
+    it { refute user.can_view?(StateAidLetter) }
   end
 
   context 'premium schedules' do
-    it { refute can_view?(PremiumSchedule) }
-    it { refute can_update?(PremiumSchedule) }
+    it { refute user.can_view?(PremiumSchedule) }
+    it { refute user.can_update?(PremiumSchedule) }
   end
 
   context 'Loan Offer' do
-    it { refute can_create?(LoanOffer) }
+    it { refute user.can_create?(LoanOffer) }
   end
 
   context 'Loan Entry' do
-    it { refute can_create?(LoanEntry) }
+    it { refute user.can_create?(LoanEntry) }
   end
 
   context 'Loan Cancel' do
-    it { refute can_create?(LoanCancel) }
+    it { refute user.can_create?(LoanCancel) }
   end
 
   context 'Loan Demand to Borrower' do
-    it { refute can_create?(LoanDemandToBorrower) }
+    it { refute user.can_create?(LoanDemandToBorrower) }
   end
 
   context 'Loan Repay' do
-    it { refute can_create?(LoanRepay) }
+    it { refute user.can_create?(LoanRepay) }
   end
 
   context 'Loan No Claim' do
-    it { refute can_create?(LoanNoClaim) }
+    it { refute user.can_create?(LoanNoClaim) }
   end
 
   context 'Loan Demand Against Government' do
-    it { refute can_create?(LoanDemandAgainstGovernment) }
+    it { refute user.can_create?(LoanDemandAgainstGovernment) }
   end
 
   context 'Loan Guarantee' do
-    it { refute can_create?(LoanGuarantee) }
+    it { refute user.can_create?(LoanGuarantee) }
   end
 
   context 'loan changes' do
-    it { refute can_view?(LoanChange) }
-    it { refute can_create?(LoanChange) }
+    it { refute user.can_view?(LoanChange) }
+    it { refute user.can_create?(LoanChange) }
   end
 
   context 'loan alerts' do
-    it { assert can_view?(LoanAlerts) }
+    it { assert user.can_view?(LoanAlerts) }
   end
 
   context 'premium schedule report' do
-    it { refute can_create?(PremiumScheduleReport) }
+    it { refute user.can_create?(PremiumScheduleReport) }
   end
 
   context 'Loan' do
-    it { assert can_view?(Loan) }
+    it { assert user.can_view?(Loan) }
   end
 
   context 'Loan::States' do
-    it { assert can_view?(Loan::States) }
+    it { assert user.can_view?(Loan::States) }
   end
 
   context 'Search' do
-    it { assert can_view?(Search) }
+    it { assert user.can_view?(Search) }
   end
 end
