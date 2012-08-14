660c9bff4a571deeec9510dbd014f535a68e650b
Ben Pickles <spideryoung@gmail.com>
2012-08-14 16:32:42 +0100

    Populate existing systems' loan creator/modifier with dummy data and enforce not null constraints on loans created_by_id and modified_by_id.

--- db/migrate/20120813110926_add_creator_and_modifier_columns_to_loans.rb
+++ db/migrate/20120813110926_add_creator_and_modifier_columns_to_loans.rb
@@ -1,7 +1,20 @@
 class AddCreatorAndModifierColumnsToLoans < ActiveRecord::Migration
-  def change
-    # TODO: Make these not nullable.
-    add_column :loans, :created_by_id, :integer  #, null: false
-    add_column :loans, :modified_by_id, :integer #, null: false
+  def up
+    add_column :loans, :created_by_id, :integer
+    add_column :loans, :modified_by_id, :integer
+
+    user_id = User.first.id
+    Loan.update_all(
+      created_by_id: user_id,
+      modified_by_id: user_id
+    )
+
+    change_column_null :loans, :created_by_id, false
+    change_column_null :loans, :modified_by_id, false
+  end
+
+  def down
+    remove_column :loans, :created_by_id
+    remove_column :loans, :modified_by_id
   end
 end
