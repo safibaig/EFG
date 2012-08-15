c08778b5617f0514c6a3b02a4e3813584111c951
Ben Pickles <spideryoung@gmail.com>
2012-08-15 17:19:12 +0100

    Already testing this.

--- spec/lib/importers/lender_importer_spec.rb
+++ spec/lib/importers/lender_importer_spec.rb
@@ -9,37 +9,6 @@ describe LenderImporter do
     Faker::Company.stub!(:name).and_return('ACME')
   end
 
-  describe "#attributes" do
-    let(:row) { CSV.read(csv_fixture_path, headers: true).first }
-    let(:importer) { LenderImporter.new(row) }
-
-    it "should return a hash of attributes" do
-      importer.attributes.should == {
-        legacy_id: "465",
-        name: "ACME",
-        created_at: "01-APR-10",
-        updated_at: "11-MAY-10",
-        version: "1",
-        high_volume: "1",
-        can_use_add_cap: "1",
-        organisation_reference_code: "LC",
-        primary_contact_name: "PRIMARY CONTACT NAME",
-        primary_contact_phone: "01010101010101010101",
-        primary_contact_email: "emailaddress@emailaddress.emailaddress",
-        std_cap_lending_allocation: "123",
-        add_cap_lending_allocation: "456",
-        disabled: "0",
-        ar_timestamp: "11-DEC-06",
-        ar_insert_timestamp: "20-MAR-07",
-        created_by: "B0A11CFA070A9EB86882BB5199645F1232C47F8E",
-        modified_by: "8CD950DED80063E4502DDAD23C4A0D7EF03B7EE3",
-        allow_alert_process: "1",
-        main_point_of_contact_user: "CC0312A05920C0BC0204CB65162AD1B9F5C94033",
-        loan_scheme: "E"
-      }
-    end
-  end
-
   describe ".import" do
     def dispatch
       LenderImporter.csv_path = csv_fixture_path
