6ce7b336624d1b9b669a44330a330876f29d9d9a
Ben Pickles <spideryoung@gmail.com>
2012-08-15 13:39:51 +0100

    Emails are no longer unique.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120814091718) do
+ActiveRecord::Schema.define(:version => 20120815123757) do
 
   create_table "invoices", :force => true do |t|
     t.integer  "lender_id"
@@ -370,7 +370,6 @@ ActiveRecord::Schema.define(:version => 20120814091718) do
     t.string   "type"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["type"], :name => "index_users_on_type"
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
