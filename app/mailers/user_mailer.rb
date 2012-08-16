5b2f8d5d01e74591974185c4f1f0eee4d5d8bea7
Niall Mullally <niall.mullally@gmail.com>
2012-08-16 15:59:06 +0100

    Use Devise.mailer_sender as from address for UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,7 @@
 class UserMailer < ActionMailer::Base
 
+  default :from => Devise.mailer_sender
+
   def new_account_notification(user)
     @user = user
     mail(to: user.email, subject: "Please login to your new EFG account")
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -19,6 +19,10 @@ describe UserMailer do
     it "should contain link to reset password page" do
       @email.body.should match(/\?reset_password_token=#{@user.reset_password_token}/)
     end
+
+    it "should have a from header" do
+      @email.from.should == [ Devise.mailer_sender ]
+    end
   end
 
 end
