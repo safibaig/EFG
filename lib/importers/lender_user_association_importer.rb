4363b12757b557be1eec8f9ca55946a9cb8736d4
Ben Pickles <spideryoung@gmail.com>
2012-08-16 12:14:43 +0100

    Don't display the progress bar during tests.

--- lib/importers/lender_user_association_importer.rb
+++ lib/importers/lender_user_association_importer.rb
@@ -1,15 +1,18 @@
 class LenderUserAssociationImporter
   def self.import
-    progress_bar = ProgressBar.new('Lender User Association', Lender.count())
-
     Lender.all.each.with_index do |lender, index|
       lender.created_by  = User.find_by_username!(lender.created_by_legacy_id)  if lender.created_by_legacy_id.present?
       lender.modified_by = User.find_by_username!(lender.modified_by_legacy_id) if lender.modified_by_legacy_id.present?
       lender.save!
 
-      progress_bar.set(index)
+      progress_bar.try(:set, index)
     end
 
-    progress_bar.finish
+    progress_bar.try(:finish)
+  end
+
+  def self.progress_bar
+    return nil if Rails.env.test?
+    @progress_bar ||= ProgressBar.new('Lender User Association', Lender.count())
   end
 end
