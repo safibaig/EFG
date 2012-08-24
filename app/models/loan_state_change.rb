1b6e45787f524eedf31dac770434603d852770ef
Niall Mullally <niall.mullally@gmail.com>
2012-08-24 12:12:33 +0100

    LoanStateChange updates

--- app/models/loan_state_change.rb
+++ app/models/loan_state_change.rb
@@ -5,4 +5,10 @@ class LoanStateChange < ActiveRecord::Base
 
   validates_presence_of :loan_id, :state, :event_id, :modified_on, :modified_by_id
 
+  attr_accessible :loan_id, :state, :modified_on, :modified_by, :event_id
+
+  def event
+    LoanEvent.find(event_id)
+  end
+
 end
