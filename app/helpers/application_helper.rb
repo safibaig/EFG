cd924fd381d89ff67549db602b092efc4f12e42a
Ben Pickles <spideryoung@gmail.com>
2012-09-04 10:36:15 +0100

    Add Google Analytics tracking code for preview and production environments.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,25 @@ module ApplicationHelper
   def current_user_access_restricted?
     current_user.disabled? || current_user.locked?
   end
+
+  def google_analytics
+    return unless Rails.env.production?
+
+    account, domain = request.host.include?('.preview.') ?
+      ['UA-34504094-2', 'preview.alphagov.co.uk'] :
+      ['UA-34504094-1', 'production.alphagov.co.uk']
+
+    %Q(<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '#{account}']);
+  _gaq.push(['_setDomainName', '#{domain}']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>).html_safe
+  end
 end
