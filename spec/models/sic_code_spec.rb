3e573e3dd9da1ed08dcc094acd35779a6cde90ec
Niall Mullally <niall.mullally@gmail.com>
2012-09-06 13:10:11 +0100

    Change SicCode#code to be unique

--- spec/models/sic_code_spec.rb
+++ spec/models/sic_code_spec.rb
@@ -9,9 +9,18 @@ describe SicCode do
       sic_code.should be_valid
     end
 
-    it 'requires a code' do
-      sic_code.code = nil
-      sic_code.should_not be_valid
+    describe "#code" do
+      let(:another_sic_code) { FactoryGirl.build(:sic_code, code: sic_code.code) }
+
+      it 'cannot be blank' do
+        sic_code.code = nil
+        sic_code.should_not be_valid
+      end
+
+      it 'must be unique' do
+        sic_code.save
+        another_sic_code.should_not be_valid
+      end
     end
 
     it 'requires a description' do
