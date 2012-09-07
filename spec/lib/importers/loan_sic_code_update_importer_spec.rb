993deeb268037276993d40f6f08fb59b4b0b0def
Niall Mullally <niall.mullally@gmail.com>
2012-09-07 10:27:09 +0100

    Update LoanSicCodeUpdateImporter specs

--- spec/lib/importers/loan_sic_code_update_importer_spec.rb
+++ spec/lib/importers/loan_sic_code_update_importer_spec.rb
@@ -6,7 +6,18 @@ describe LoanSicCodeUpdateImporter do
   let(:csv_fixture_path) { Rails.root.join('spec/fixtures/import_data/loan_sic_code_mapping.csv') }
 
   describe ".import" do
-    let!(:loan) { FactoryGirl.create(:loan, reference: 'GP9GR7V+01', sic_code: 'A01.11.001') }
+    let!(:loan) {
+      FactoryGirl.create(
+        :loan,
+        reference: 'GP9GR7V+01',
+        sic_code: 'A01.11.001',
+        legacy_sic_code: 'A01.11.001',
+        legacy_sic_desc: 'Legacy description',
+        legacy_sic_eligible: false,
+        legacy_sic_parent_desc: 'Legacy parent description',
+        legacy_sic_notified_aid: true,
+      )
+    }
 
     let!(:sic_code) {
       FactoryGirl.create(
@@ -32,9 +43,20 @@ describe LoanSicCodeUpdateImporter do
       loan.reload
       loan.sic_code.should == "01110"
       loan.sic_desc.should == "Growing of cereals (except rice)"
-      loan.should be_sic_eligible
+      loan.sic_eligible.should == true
       loan.sic_parent_desc.should be_nil
       loan.sic_notified_aid.should be_nil
+    end
+
+    it "should not change legacy SIC data" do
+      dispatch
+
+      loan.reload
+      loan.legacy_sic_code.should == 'A01.11.001'
+      loan.legacy_sic_desc.should == 'Legacy description'
+      loan.legacy_sic_eligible.should == false
+      loan.legacy_sic_parent_desc.should == 'Legacy parent description'
+      loan.legacy_sic_notified_aid.should == true
     end
 
     it "should not change created_at and updated_at timestamps" do
