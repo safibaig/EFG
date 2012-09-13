b2373253f5f86d1066e6c0903a49f4aa40d1c54e
Niall Mullally <niall.mullally@gmail.com>
2012-09-13 11:12:09 +0100

    Update search form labels

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -6,4 +6,10 @@ module SearchHelper
   def modified_by_options
     current_lender.lender_users.order_by_username.map { |user| [ user.username, user.id ] }
   end
+
+  def search_sort_options
+    Search::SortableAttributes.map do |attribute|
+      [ I18n.t("sort_#{attribute}", scope: [:simple_form, :labels, :search]), attribute ]
+    end
+  end
 end
