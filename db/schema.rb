db815cbe09b70706d8cf423253ec4cbf254d94c9
Ben Pickles <spideryoung@gmail.com>
2012-09-14 16:33:34 +0100

    Fix LoanModification types for everyone else.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120911100906) do
+ActiveRecord::Schema.define(:version => 20120914152943) do
 
   create_table "ded_codes", :force => true do |t|
     t.string   "legacy_id"
