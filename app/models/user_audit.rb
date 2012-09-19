90c1e2bc377aa2e82aeb7775a7a55e001c8afd4a
Niall Mullally <niall.mullally@gmail.com>
2012-09-19 16:26:55 +0100

    UserAudit validations

--- app/models/user_audit.rb
+++ app/models/user_audit.rb
@@ -1,4 +1,11 @@
 class UserAudit < ActiveRecord::Base
+
+  INITIAL_LOGIN = "Initial login"
+
   belongs_to :user
   belongs_to :modified_by, class_name: "User"
+
+  attr_accessible :user_id, :function, :modified_by, :password
+
+  validates_presence_of :user_id, :function, :modified_by_id, :password
 end
--- spec/models/user_audit_spec.rb
+++ spec/models/user_audit_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe UserAudit do
+
+  describe "validations" do
+    let(:user_audit) { FactoryGirl.build(:user_audit) }
+
+    it "should have a valid factory" do
+      user_audit.should be_valid
+    end
+
+    it "must have a user_id" do
+      user_audit.user_id = nil
+      user_audit.should_not be_valid
+    end
+
+    it "must have a function" do
+      user_audit.function = nil
+      user_audit.should_not be_valid
+    end
+
+    it "must have a modified_by_id" do
+      user_audit.modified_by_id = nil
+      user_audit.should_not be_valid
+    end
+
+    it "must have a password" do
+      user_audit.password = nil
+      user_audit.should_not be_valid
+    end
+  end
+
 end
