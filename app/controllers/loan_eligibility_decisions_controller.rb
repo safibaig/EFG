e1363e160a4bbab0433d01cb0bc7c5f74d3aba43
Niall Mullally <niall.mullally@gmail.com>
2012-09-21 11:15:37 +0100

    Add email decision form to loan eligible view

--- app/controllers/loan_eligibility_decisions_controller.rb
+++ app/controllers/loan_eligibility_decisions_controller.rb
@@ -1,7 +1,29 @@
 class LoanEligibilityDecisionsController < ApplicationController
 
+  before_filter :find_loan
+
   def show
-    @loan = current_lender.loans.where(state: [ Loan::Eligible, Loan::Rejected ]).find(params[:loan_id])
+    if @loan.state == Loan::Eligible
+      @eligibility_decision_email = EligibilityDecisionEmail.new
+      render "eligible"
+    else
+      render "ineligible"
+    end
+  end
+
+  def email
+    @eligibility_decision_email = EligibilityDecisionEmail.new(params[:eligibility_decision_email])
+    if @eligibility_decision_email.valid?
+      # send email
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def find_loan
+    @loan = current_lender.loans.where(state: Loan::Eligible).find(params[:loan_id])
   end
 
 end
