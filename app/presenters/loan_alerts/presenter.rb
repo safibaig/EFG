dfbd642cf0cdc5f3aa3e082af0d1da5ac3eac632
Niall Mullally <niall.mullally@gmail.com>
2012-10-17 17:04:10 +0100

    'View all' link in loan alerts

--- app/presenters/loan_alerts/presenter.rb
+++ app/presenters/loan_alerts/presenter.rb
@@ -9,16 +9,14 @@ module LoanAlerts
 
     def alerts_grouped_by_priority
       [
-        Group.new(@high_priority_loans, :high, max_loan_count),
-        Group.new(@medium_priority_loans, :medium, max_loan_count),
-        Group.new(@low_priority_loans, :low, max_loan_count)
+        Group.new(@high_priority_loans, :high, total_loan_count),
+        Group.new(@medium_priority_loans, :medium, total_loan_count),
+        Group.new(@low_priority_loans, :low, total_loan_count)
       ]
     end
 
-    private
-
-    def max_loan_count
-      @max_loan_count ||= [
+    def total_loan_count
+      @total_loan_count ||= [
         @high_priority_loans.collect(&:length).max,
         @medium_priority_loans.collect(&:length).max,
         @low_priority_loans.collect(&:length).max
