f9c11ab9f68dcf100a0155e64fd42104c251faa2
Niall Mullally <niall.mullally@gmail.com>
2012-10-19 12:31:22 +0100

    Update new account and reset password email text

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,7 +4,7 @@ describe UserMailer do
 
   describe "#new_account_notification" do
     before do
-      @user = FactoryGirl.build(:user, reset_password_token: 'abc123')
+      @user = FactoryGirl.build(:lender_user, reset_password_token: 'abc123', first_name: 'Joe', username: 'joe123')
       @email = UserMailer.new_account_notification(@user)
     end
 
@@ -13,21 +13,21 @@ describe UserMailer do
     end
 
     it "should contain user's first name" do
-      @email.body.should include(@user.first_name)
+      @email.body.should include('joe')
     end
 
     it "should contain user's username" do
-      @email.body.should include(@user.username)
+      @email.body.should include('joe123')
     end
 
     it "should contain link to reset password page" do
       @email.body.should include("?reset_password_token=#{@user.reset_password_token}")
     end
-    
+
     it "should contain a link back to the home page to resend the request" do
-      @email.body.should include(%Q("#{root_url}"))
+      @email.body.should include(root_url)
     end
-    
+
     it "should have a from header" do
       Devise.mailer_sender.should match @email.from[0]
     end
