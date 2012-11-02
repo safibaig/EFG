8999086eaca2e9385247bd2c87c9579fad8c898e
Niall Mullally <niall.mullally@gmail.com>
2012-11-02 11:04:51 +0000

    Fix failing loan alert spec

--- spec/presenters/loan_alerts/priority_grouping_spec.rb
+++ spec/presenters/loan_alerts/priority_grouping_spec.rb
@@ -32,47 +32,47 @@ describe LoanAlerts::PriorityGrouping do
   end
 
   describe "#groups_hash" do
-    Timecop.freeze(Date.parse("26-10-2012")) do
-      let!(:high_priority_loan1) { FactoryGirl.create(:loan, :offered, maturity_date: 59.weekdays_ago(Date.today)) }
+    let(:date) { Date.parse("31-10-2012") }
 
-      let!(:high_priority_loan2) { FactoryGirl.create(:loan, :offered, maturity_date: 50.weekdays_ago(Date.today)) }
+    let!(:high_priority_loan1) { FactoryGirl.create(:loan, :offered, maturity_date: 59.weekdays_ago(date)) }
 
-      let!(:medium_priority_loan1) { FactoryGirl.create(:loan, :offered, maturity_date: 49.weekdays_ago(Date.today)) }
+    let!(:high_priority_loan2) { FactoryGirl.create(:loan, :offered, maturity_date: 50.weekdays_ago(date)) }
 
-      let!(:medium_priority_loan2) { FactoryGirl.create(:loan, :offered, maturity_date: 30.weekdays_ago(Date.today)) }
+    let!(:medium_priority_loan1) { FactoryGirl.create(:loan, :offered, maturity_date: 49.weekdays_ago(date)) }
 
-      let!(:low_priority_loan1) { FactoryGirl.create(:loan, :offered, maturity_date: 29.weekdays_ago(Date.today)) }
+    let!(:medium_priority_loan2) { FactoryGirl.create(:loan, :offered, maturity_date: 30.weekdays_ago(date)) }
 
-      let!(:low_priority_loan2) { FactoryGirl.create(:loan, :offered, maturity_date: Date.today) }
+    let!(:low_priority_loan1) { FactoryGirl.create(:loan, :offered, maturity_date: 29.weekdays_ago(date)) }
 
-      let(:priority_grouping) {
-        loans_array = [
-          high_priority_loan1,
-          high_priority_loan2,
-          medium_priority_loan1,
-          medium_priority_loan2,
-          low_priority_loan1,
-          low_priority_loan2
-        ]
-        LoanAlerts::PriorityGrouping.new(loans_array, 59.weekdays_ago(Date.today).to_date, Date.today, :maturity_date)
-      }
+    let!(:low_priority_loan2) { FactoryGirl.create(:loan, :offered, maturity_date: date) }
 
-      it "should return hash of loan arrays grouped by high priority, medium priority and low priority, skipping weekends" do
-        priority_grouping.groups_hash[:high_priority].should == padded_array_of_arrays(10, {
-          0 => [high_priority_loan1],
-          9 => [high_priority_loan2]
-        })
+    let(:priority_grouping) {
+      loans_array = [
+        high_priority_loan1,
+        high_priority_loan2,
+        medium_priority_loan1,
+        medium_priority_loan2,
+        low_priority_loan1,
+        low_priority_loan2
+      ]
+      LoanAlerts::PriorityGrouping.new(loans_array, 59.weekdays_ago(date).to_date, date, :maturity_date)
+    }
 
-        priority_grouping.groups_hash[:medium_priority].should == padded_array_of_arrays(20, {
-          0 => [medium_priority_loan1],
-          19 => [medium_priority_loan2]
-        })
+    it "should return hash of loan arrays grouped by high priority, medium priority and low priority, skipping weekends" do
+      priority_grouping.groups_hash[:high_priority].should == padded_array_of_arrays(10, {
+        0 => [high_priority_loan1],
+        9 => [high_priority_loan2]
+      })
 
-        priority_grouping.groups_hash[:low_priority].should == padded_array_of_arrays(30, {
-          0 => [low_priority_loan1],
-          29 => [low_priority_loan2]
-        })
-      end
+      priority_grouping.groups_hash[:medium_priority].should == padded_array_of_arrays(20, {
+        0 => [medium_priority_loan1],
+        19 => [medium_priority_loan2]
+      })
+
+      priority_grouping.groups_hash[:low_priority].should == padded_array_of_arrays(30, {
+        0 => [low_priority_loan1],
+        29 => [low_priority_loan2]
+      })
     end
   end
 
