1eb9da518a680a7e1abb9ec83f6009d801d82b46
Olly Legg <olly@new-bamboo.co.uk>
2012-11-06 14:11:48 +0000

    Configure the flag to properly determine whether we are in training mode.

--- config/initializers/training_mode.rb
+++ config/initializers/training_mode.rb
@@ -1,4 +1,5 @@
-EFG::Application.config.training_mode = true
+# Flag used to indicate whether this is the training app or not. CSS can be different, etc.
+EFG::Application.config.training_mode = (ENV["EFG_HOST"] =~ /training/)
 
 if EFG::Application.config.training_mode
   ActionMailer::Base.send(:include, TrainingMailer)
