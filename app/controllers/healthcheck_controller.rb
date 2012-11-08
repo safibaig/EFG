127021387fe8cdb56d86526d6f2cc2fe8a4edea0
James Abley <james.abley@gmail.com>
2012-11-08 12:02:24 +0000

    Include mailer diagnostics in healthcheck

--- app/controllers/healthcheck_controller.rb
+++ app/controllers/healthcheck_controller.rb
@@ -2,6 +2,9 @@ class HealthcheckController < ActionController::Base
   def index
     # Check database connectivity
     Lender.count
-    render json: { git_sha1: CURRENT_RELEASE_SHA }
+    render json: {
+      git_sha1: CURRENT_RELEASE_SHA,
+      mailer_config: EFG::Application.config.action_mailer.default_url_options,
+    }
   end
 end
