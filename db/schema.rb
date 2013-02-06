826c0651552d79b0ce3d0286677b257d0294bcdf
Olly Legg <olly@new-bamboo.co.uk>
2013-02-06 14:32:30 +0000

    Add migration to assign the correct phases to some of the lending limits.
    Based on information from Karl.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130201105903) do
+ActiveRecord::Schema.define(:version => 20130206141611) do
 
   create_table "admin_audits", :force => true do |t|
     t.string   "auditable_type",        :null => false
