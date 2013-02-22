b3426a77a4d3a98f70ee1d6110063eafc5ef84af
James Abley <james.abley@gmail.com>
2013-02-22 11:28:33 +0000

    Add test for database interaction

--- spec/controllers/healthcheck_controller_spec.rb
+++ spec/controllers/healthcheck_controller_spec.rb
@@ -6,5 +6,12 @@ describe HealthcheckController do
       get :index
       response.should be_success
     end
+
+    it "should fail when the database isn't available" do
+      Lender.stub(:count).and_raise(Mysql2::Error.new("Database unavailable"))
+      assert_raise Mysql2::Error do
+        get :index
+      end
+    end
   end
 end
