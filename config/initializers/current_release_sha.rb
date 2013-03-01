af63a922b9932f6800d503560a61eb35e3b4a468
Ben Pickles <spideryoung@gmail.com>
2013-03-01 16:43:26 +0000

    Neater.

--- config/initializers/current_release_sha.rb
+++ config/initializers/current_release_sha.rb
@@ -1,6 +1,3 @@
-if File.exists?("#{Rails.root}/REVISION")
-  revision = File.open("#{Rails.root}/REVISION") { |f| f.readline.chomp }
-  CURRENT_RELEASE_SHA = revision[0..7] # Just get the short SHA
-else
-  CURRENT_RELEASE_SHA = "development"
-end
+revision = Rails.root.join('REVISION')
+
+CURRENT_RELEASE_SHA = revision.exist? ? revision.read(8) : 'development'
