a5e9d6b8d3831e280970b934b3ab3b6a151248f5
Tom Wey <tom@new-bamboo.co.uk>
2013-03-06 15:52:47 +0000

    Refactor user filter and permission code into common UsersController

--- app/controllers/lender_admins_controller.rb
+++ app/controllers/lender_admins_controller.rb
@@ -1,13 +1,4 @@
-class LenderAdminsController < ApplicationController
-  before_filter :verify_create_permission, only: [:new, :create]
-  before_filter :verify_update_permission, only: [:edit, :update, :reset_password]
-  before_filter :verify_view_permission, only: [:index, :show]
-  before_filter :verify_enable_permission, only: [:enable]
-  before_filter :verify_disable_permission, only: [:disable]
-  before_filter :verify_unlock_permission, only: [:unlock]
-
-  before_filter :find_user, only: [:show, :edit, :update, :reset_password, :unlock, :disable, :enable]
-
+class LenderAdminsController < UsersController
   def index
     params[:disabled] ||= '0'
 
@@ -79,13 +70,11 @@ class LenderAdminsController < ApplicationController
   end
 
   private
-    %w(create update view enable disable unlock).each do |action|
-      define_method :"verify_#{action}_permission" do
-        send :"enforce_#{action}_permission", LenderAdmin
-      end
-    end
-
     def find_user
       @user = LenderAdmin.where(lender_id: current_user.lender_ids).find(params[:id])
+    end
+
+    def user_class
+      LenderAdmin
     end
 end
