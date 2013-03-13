9c7e9c1d615ea5e44d9440037345b35e94c015f9
Tom Wey <tom@new-bamboo.co.uk>
2013-03-13 11:26:28 +0000

    Refactor UserHelper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,15 +1,8 @@
 module UserHelper
   def polymorphic_user_path(user, action=nil)
-    path_components = dynamically_nested_user(user)
+    path_components = [user]
+    path_components.unshift(user.lender) if [LenderUser, LenderAdmin].include? user.class
     path_components.unshift(action) if action
     polymorphic_path path_components
-  end
-
-  private
-
-  def dynamically_nested_user(user)
-    components = [user]
-    components.unshift(user.lender) if [LenderUser, LenderAdmin].include? user.class
-    components
   end
 end
