bd8596970a60db12f62a5d6014c719dac8f4af13
Tom Wey <tom@new-bamboo.co.uk>
2013-03-19 15:40:45 +0000

    Pad premium schedule report row premium amounts with nil when < 40 premium payments

--- app/reports/premium_schedule_report_row.rb
+++ app/reports/premium_schedule_report_row.rb
@@ -39,7 +39,9 @@ class PremiumScheduleReportRow
   def premiums
     array = premium_schedule.subsequent_premiums.map(&:to_f)
     array.unshift(0.0) unless premium_schedule.reschedule?
-    array
+
+    # Pad with nil when there are fewer premium payments than the maximum of 40
+    array.fill(array.length...40) { nil }
   end
 
   # always display calc_type 'N' as 'S' in this report
