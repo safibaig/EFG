0b3cc4890126ec623ceb01774ef9310bc8d89812
Olly Legg <olly@new-bamboo.co.uk>
2013-03-19 09:30:11 +0000

    Stub ExceptionNotifier delivery
    We don't have it configured in the test environment and latest mail gem is
    stricter about allowing mails without a recipient.

--- spec/support/shared_examples/rescue_from_incorrect_loan_state_error_shared_examples.rb
+++ spec/support/shared_examples/rescue_from_incorrect_loan_state_error_shared_examples.rb
@@ -1,13 +1,15 @@
 shared_examples_for 'rescue_from LoanStateTransition::IncorrectLoanState controller' do
   let(:current_user) { FactoryGirl.create(:lender_user, lender: loan.lender) }
+  let(:mail) { double('mail', deliver: true) }
 
   before do
+    ExceptionNotifier::Notifier.stub!(:exception_notification).and_return(mail)
+
     loan.update_attribute :state, 'incorrect state'
     sign_in(current_user)
   end
 
   it "should deliver the exception" do
-    mail = double('mail')
     ExceptionNotifier::Notifier.should_receive(:exception_notification).and_return(mail)
     mail.should_receive(:deliver)
 
